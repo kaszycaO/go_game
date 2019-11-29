070f9a4b22304be96d18fb5a7b548852c7a1e053
Oliwier Kaszyca <o_kaszyca@interia.pl>
2019-11-29 18:45:19 +0100

    +Board Class

--- go_game/src/main/java/tp/project/go_game/gui/MainFrame.java
+++ go_game/src/main/java/tp/project/go_game/gui/MainFrame.java
@@ -1,5 +1,7 @@
 package tp.project.go_game.gui;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 
 public class MainFrame extends JFrame {
@@ -8,11 +10,15 @@ public class MainFrame extends JFrame {
 	public MainFrame() {
 		
 		setBounds(500,500,100,100);
+		setLayout(new BorderLayout());
+		Board myBoard = new Board();
 		
 		
 		
 		
 		
+		
+		add(myBoard);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 		setResizable(false);
