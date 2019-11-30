5d979d8e2cdd95042500fb827144096573544e62
Dominika Szydlo <dszydlo28@gmail.com>
2019-11-30 18:33:02 +0100

    bad gui

--- go_game/src/main/java/tp/project/go_game/gui/GUIAdapter.java
+++ go_game/src/main/java/tp/project/go_game/gui/GUIAdapter.java
@@ -47,20 +47,6 @@ public class GUIAdapter extends MouseAdapter implements ActionListener {
 	
 	@Override
 	public void mousePressed(MouseEvent event) {
-
-	
-		coordinatesConverter(event.getX(), event.getY());
-		
-		if(squareX > 0 && squareY >0 ) {
-			
-		drawConverter(squareX, squareY);	
-		myFrame.myBoard.repaint();
-		System.out.println("Kwadrat: "+squareX+" "+squareY);
-		//TODO naprawic dolna krawedz
-		
-		}
-
-    
 		try {
 	           socket = new Socket("localhost", 4444);
 	           toServer = new DataOutputStream(socket.getOutputStream());
@@ -80,44 +66,20 @@ public class GUIAdapter extends MouseAdapter implements ActionListener {
 	            System.out.println(e.getMessage());
 	            System.exit(1);
 	        }
+		coordinatesConverter(event.getX(), event.getY());
+		
+		if(squareX > 0 && squareY >0 ) {
+			
+			drawConverter(squareX, squareY);	
+			myFrame.myBoard.repaint();
+			System.out.println("Kwadrat: "+squareX+" "+squareY);
+		//TODO naprawic dolna krawedz
+		
+		}
+
+    
 	}
 
-	
-
-	public void coordinatesConverter(int X, int Y) {
-		
-		int squareSize = 840/(myFrame.getBoardSize() + 1) ; 
-		int newX = X + squareSize/2;
-		int newY = Y + squareSize/2;
-		
-		int squareX = newX/squareSize;
-		int squareY = newY/squareSize;
-		
-		System.out.println(squareX);
-		System.out.println(squareY); 
-			
-			setSquareX(squareX);
-			setSquareY(squareY);
-			
-		
-			
-		
-	}
-	
-	public void drawConverter(int X, int Y) {
-		
-		int squareSize = 840/(myFrame.getBoardSize() + 1); 
-		setDrawX(X * squareSize);
-		setDrawY(Y * squareSize);
-		
-  }
-		
-	
-		
-	
-	
-	
-	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
@@ -144,7 +106,31 @@ public class GUIAdapter extends MouseAdapter implements ActionListener {
 	            System.exit(1);
 	        }
 	}
+
+	public void coordinatesConverter(int X, int Y) {
+		
+		int squareSize = 840/(myFrame.getBoardSize() + 1) ; 
+		int newX = X + squareSize/2;
+		int newY = Y + squareSize/2;
+		
+		int squareX = newX/squareSize;
+		int squareY = newY/squareSize;
+		
+		System.out.println(squareX);
+		System.out.println(squareY); 
+			
+		setSquareX(squareX);
+		setSquareY(squareY);
+	}
 	
+	public void drawConverter(int X, int Y) {
+		
+		int squareSize = 840/(myFrame.getBoardSize() + 1); 
+		setDrawX(X * squareSize);
+		setDrawY(Y * squareSize);
+		
+  }
+		
 	public void setSquareX(int X) {
 		
 		this.squareX = X;
